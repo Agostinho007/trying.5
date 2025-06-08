a2512574487917f3448e3a45b8e1195275dcc89e
Agostinho007 <706230095@ucm.ac.mz>
2025-06-08 20:53:45 +0200

    Going

---
+++ js/script.js
@@ -0,0 +1,61 @@
+document.getElementById('formUser').addEventListener('submit', async function(event) {
+    event.preventDefault();
+
+    const novouser = {
+        id: Date.now(),
+        name: document.getElementById('name').value,
+        idade: parseInt(document.getElementById('idade').value),
+        cidade: document.getElementById('cidade').value
+    }
+
+    try {
+        const responsee = await fetch ('http://localhost:3000/api/users', {
+            method: 'post', 
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(novouser)
+        });
+
+        if(!responsee.ok){
+            console.log('Erro na Conexao', responsee.status);
+        }
+
+        const respostaa = await responsee.json();
+        console.log('User Added: ', respostaa)
+
+        const lista = document.getElementById('alert');
+
+        lista.innerHTML += `
+            <p>O user ${novouser.name} foi adicionado</p>`
+        document.getElementById('formUser').reset();
+    } catch (error) {
+        console.error('Erro: ', error);
+    }
+})
+
+async function carregarUsers() {
+    try {
+        const lista = document.getElementById('lista');
+        const respons = await fetch ('server/database.json');
+
+        if (!respons.ok) {
+            console.log('Erro de Conexao');
+        } else {
+            console.log('Conectado');
+            const data = await respons.json();
+            const users = data.users;
+
+            users.forEach(user => {
+                lista.innerHTML += `
+                    <h3>${user.name}</h3>
+                    <p>${user.idade}</p>
+                    <p>${user.cidade}</p>`  
+            });
+        }
+    } catch (error) {
+        console.error('Erro:', error);
+    }
+    
+}
+
---
+++ server/app.js
@@ -0,0 +1,84 @@
+const express = require('express');
+const fs = require('fs');
+const WebSocket = require('ws');
+const cors = require('cors');
+const PORT = process.env.PORT || 3000;
+
+const application = express();
+application.use(express.json());
+application.use(express.static('.'));
+
+application.use(cors({
+    origin: ' * ',
+}))
+
+const wss = new WebSocket.Server({port: 3001})
+
+function lerDados(){
+    try {
+        return JSON.parse(fs.readFileSync('server/database.json'));
+    } catch (error) {
+        console.error('Erro ao Ler Dados: ', error);
+        return {
+            users: [], mensagens: []
+        }
+    }
+}
+
+function salvarDados(dados){
+    try {
+        fs.writeFileSync('server/database.json',  JSON.stringify(dados, null, 2));
+    } catch (error) {
+        console.error('Erro ao salvar dados em database.json: ', error);
+    }
+}
+
+wss.on('connection', (ws) => {
+    console.log('Novo Cliente Concectado!');
+    const dados = lerDados();
+    ws.send(JSON.stringify({tipo: 'historico', mensagens: dados.mensagens}));
+
+
+    ws.on('message', (mensagem) => {
+    const dados = lerDados();
+    const novaMensagem = JSON.parse(mensagem);
+    dados.mensagens.push({
+        id: Date.now(),
+        texto: novaMensagem.texto,
+        usuario: novaMensagem.usuario||'Anonimo',
+        timestamp: new Date().toISOString()
+    });
+    salvarDados(dados);
+    wss.clients.forEach((client) => {
+        if(client.readyState === WebSocket.OPEN) {
+            client.send(JSON.stringify({
+                tipo: 'novaMensagem', 
+                mensagem: dados.mensagens[dados.mensagens.length -1 ]
+            }))
+        }
+    })
+
+    ws.on('close', () => {
+        console.log('Cliente Desconectado!');
+    });
+});});
+
+
+application.post('/api/users', (req, res) => {
+    try {
+        const novouser = req.body;
+        const dados = JSON.parse(fs.readFileSync('server/database.json'));
+        dados.users.push(novouser);
+        fs.writeFileSync('server/database.json', JSON.stringify(dados, null, 2));
+        res.status(201).json(novouser);
+    } catch (error) {
+        console.error('Erro: ', error);
+        res.status(500).json({
+            error: 'Erro no Servidor'
+        });
+    }
+})
+
+application.listen(PORT, () => {
+    console.log(`Server ON in port ${PORT} `);
+})
